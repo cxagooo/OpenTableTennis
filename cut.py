eb38696554b0aca034cdafbe0f997d96a22318c8
cxgaooo <lbenwei92@gmail.com>
2024-08-10 16:48:40 +0800

    1

--- cut.py
+++ cut.py
@@ -1,44 +1,51 @@
-import cv2
+from moviepy.editor import VideoFileClip
+from moviepy.tools import cvsecs
+from PIL import Image
 import os
+from list_change import get_data
+def extract_frames(input_path, output_dir):
+    # 创建输出目录如果不存在
 
-def extract_frames(video_path, output_dir):
-    # 打开视频文件
-    video = cv2.VideoCapture(video_path)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-    # 获取视频的总帧数
-    frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-    # 计算中间帧的位置
-    mid_frame_index = frame_count // 2
+    # 加载视频文件
 
-    # 初始化帧计数器
-    frame_counter = 0
-    n = 0
-    while True:
-        ret, frame = video.read()
-        if not ret:
-            break
-        # 检查当前帧是否为所需帧
-        if frame_counter in (0, mid_frame_index, frame_count - 1):
-            # 构建输出文件名
-            filename = f"frame_{n}.jpg"
-            filepath = output_dir + '/' + filename
-            n += 1
-            # 保存帧到文件
-            cv2.imwrite(filepath, frame)
-        frame_counter += 1
+    clip = VideoFileClip(input_path)
+
+    # 获取视频的总时长
+
+    duration = clip.duration
+
+    # 计算每张图片之间的间隔
+
+    interval = duration / 6  # 因为我们要包括首尾帧，所以是6个间隔
+
+    # 提取帧并保存为图片
+
+    for i in range(7):
+        time_point = i * interval
+
+        frame = clip.get_frame(time_point)
+
+        # 保存图片到文件
+
+        output_path = os.path.join(output_dir, f"frame_{i}.png")
+
+
+
+        im = Image.fromarray(frame)
+
+        im.save(output_path)
 
     # 释放资源
-    video.release()
+
+    clip.close()
 
 
-# 设置视频路径和输出目录
-#video_path = '/home/cxgao/Videos/pyopen/12-标记-0.mp4'
-#output_dir = 'CutFrame_Output'
 
-# 调用函数
-#extract_frames(video_path, output_dir)
-'''for i in range(24):
-    os.mkdir(f'CutFrame_Output/output{i}')
-    video_path = f'/home/cxgao/Videos/pyopen/12-标记-{i}.mp4'
-    output_dir = f'CutFrame_Output/output{i}'
-    extract_frames(video_path, output_dir)'''
+#input_video_path = "path/to/your/video.mp4"
+
+#output_directory = "path/to/output/directory"
+
+#extract_frames(input_video_path, output_directory)
--- train.py
+++ train.py
@@ -2,6 +2,7 @@ from demo import detect
 from OpenAndPick import pick
 from cut import extract_frames
 import os
+from list_change import get_data
 #截取帧
 for i in range(24):
     #os.mkdir(f'CutFrame_Output/output{i}')
@@ -9,12 +10,13 @@ for i in range(24):
     output_dir = f'CutFrame_Output/output{i}'
     extract_frames(video_path, output_dir)
 for i in range(24):
-    for j in range(3):
-        path = f'CutFrame_Output/output{i}/frame_{j}.jpg'
+    for j in range(7):
+        path = f'CutFrame_Output/output{i}/frame_{j}.png'
         print(path)
         output = f'CutFrame_Output/output{i}/tem.txt'
         outputpath = f'CutFrame_Output/output{i}/use{j}.txt'
         detect(path, output, outputpath)
+data =get_data('CutFrame_Output')
 
 
 
