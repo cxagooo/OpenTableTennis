0c3dafa63619cc755d298c59fd172a372aa3450f
cxgaooo <lbenwei92@gmail.com>
2024-08-11 12:12:11 +0800

    1

--- train.py
+++ train.py
@@ -4,12 +4,13 @@ from cut import extract_frames
 import os
 from list_change import get_data
 #截取帧
-for i in range(24):
+for i in range(57):
     #os.mkdir(f'CutFrame_Output/output{i}')
-    video_path = f'/home/cxgao/Videos/pyopen/12-标记-{i}.mp4'
-    output_dir = f'CutFrame_Output/output{i}'
+    p = i + 24
+    video_path = f'/home/cxgao/Videos/data/21-标记-{i}.mp4'
+    output_dir = f'CutFrame_Output/output{p}'
     extract_frames(video_path, output_dir)
-for i in range(24):
+for i in range(24, 81):
     for j in range(7):
         path = f'CutFrame_Output/output{i}/frame_{j}.png'
         print(path)
